85b31ea872b1c93dc2099afc14f9009f9aff6c1b
emrahelis <emrahelis@gmail.com>
2024-11-03 10:30:08 +0300

    dsfd

--- Personel.cs
+++ Personel.cs
@@ -5,4 +5,6 @@ public class Personel{
 
     public int Age { get; set; }    
 
+    public decimal Salary{ get; set; }
+
 }
--- Program.cs
+++ Program.cs
@@ -6,16 +6,18 @@
 
 
 using System.Collections;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
 List<Personel> personels = new List<Personel>();
-personels.Add(new Personel() { Id = 1, Name = "Ramazan", Age = 12 });
-personels.Add(new Personel() { Id = 2, Name = "Ali", Age = 24 });
-personels.Add(new Personel() { Id = 3, Name = "Burak", Age = 19 });
-personels.Add(new Personel() { Id = 4, Name = "Selçuk", Age = 11 });
-personels.Add(new Personel() { Id = 5, Name = "Nuray", Age = 50 });
-personels.Add(new Personel() { Id = 6, Name = "Hatice", Age = 45 });
-personels.Add(new Personel() { Id = 7, Name = "Burak", Age = 9 });
+personels.Add(new Personel() { Id = 1, Name = "Ramazan", Age = 12, Salary=10  });
+personels.Add(new Personel() { Id = 2, Name = "Ali", Age = 24, Salary=40 });
+personels.Add(new Personel() { Id = 3, Name = "Burak", Age = 19 , Salary=100});
+personels.Add(new Personel() { Id = 4, Name = "Selçuk", Age = 11 , Salary=50 });
+personels.Add(new Personel() { Id = 5, Name = "Nuray", Age = 50, Salary=80 });
+personels.Add(new Personel() { Id = 6, Name = "Hatice", Age = 45, Salary=500 });
+personels.Add(new Personel() { Id = 7, Name = "Burak", Age = 9 , Salary=10});
 
 // Lambda Operatörü : 
 
@@ -115,15 +117,15 @@ Personel[] personelListesiDizi =  personels.Where(s=>s.Name.StartsWith("a")).ToA
 
 // Select metodu : sorgulama sonucu gelen sonuç içerisinden istenilen alanları alma!!!
 // tüm listede sadece ad alanlarını alalım 
-var onlyName = personels.Select(s => s.Name).ToList();
+/*var onlyName = personels.Select(s => s.Name).ToList();
 foreach (string item in onlyName)
 {
     Console.WriteLine(item);
-}
+}*/
 // select metodu ile sonuç olaran dönecek olan tip belirlenir, bazen sınıf içerisindeki bir değişken, bazende
 // yeni bir tip dönülebilir!!
 // Örnek  : Personel listesini, Ogrenci listesine dönüştürelim 
-List<Ogrenci> ogrenciListesi = personels.Select(personel => new Ogrenci()
+/*List<Ogrenci> ogrenciListesi = personels.Select(personel => new Ogrenci()
 {
     Id = personel.Id,
     Name = personel.Name
@@ -133,3 +135,74 @@ foreach (Ogrenci o in ogrenciListesi)
 {
     Console.WriteLine("Id : {0} Ad : {1} ", o.Id, o.Name);
 }
+
+*/
+
+
+// Linq To Data : 
+
+// Linq kullanarak kullanıcı giriş doğrulama işlemi yapınız
+// Personel tablosundaki ad ve soyad bilgilerini ekrana yazdırınız
+
+
+
+/*Ogrenci o = new Ogrenci()
+{
+    Id = 1,
+    Name = "Kerim"
+
+};*/
+
+
+// Anonim tip : 
+// Aşağıda yazılı olan şey : Bir anonim  bir nesne örneğidir!!
+// Sınıf olmadan nesne örneği alabilmeyi sağlayan anonim tipe bir örnektir!!
+/*var anonim = new
+{
+
+    Id = 1,
+    Name = "Metin",
+    Age = 30
+};*/
+// az önce konuştuğumuz, select metodundan geriye farklı bir tip döndürmek istediğimizde yeni bir tip yazmamak için
+// anonim tip yazabiliriz
+//  anonim tip bir sınıf nesne örneği gibi davranır ancak sınıf yoktur!!
+
+// anonim tiplerde, property'ler istenildi kadar istenildiği tipte yazılabilir
+// propların tanımı, anonim tipi yazarken girilir
+
+// var keywordünün neden olduğunu daha iyi anlayabildiniz mi ?
+
+// Select Sorgusundan geriye bir anonim tip döndürelim !!
+/*var liste =personels.Where(s=>s.Name.Contains("a")).Select(s=>new {
+
+    Id = s.Id,
+    Name = s.Name,
+});
+
+foreach(var item in liste){
+    Console.WriteLine("Id : {0} Name : {1}",item.Id,item.Name);
+}*/
+
+// bir koleksiyon oluşturup, anonim tip ile bazı sorgular oluşturunuz
+// Id değeri 3 ve 4 olan personelin maaşı ve adı
+/*var result = personels.Where(s=>s.Id == 3 || s.Id==4).Select(s=>new {
+
+Name = s.Name,
+Salary = s.Salary
+});
+foreach(var item in result){
+    Console.WriteLine("Name : {0} Salary :{1}",item.Name,item.Salary);
+}*/
+// maaşı 50nin altında kalan personellerin  %18 zam yapılmış halde ekrana adını ve maaşını yazdırınız
+var result = personels.Where(s=>s.Salary<=50).Select(s=>new {
+    Name=s.Name,
+    Salary = s.Salary*1.18M
+});
+foreach (var personel in result){
+    Console.WriteLine("Name : {0} Salaray : {1}",personel.Name,personel.Salary);
+}
+
+
+
+
